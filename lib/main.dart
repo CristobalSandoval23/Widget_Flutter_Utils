53d449d94c07b92542b089ebe4ca81a96ead5bce
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-03-04 15:59:00 -0300

    Slimy-Card aplicado

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:widget_flutter_utils/widget/List/ListView_Widget.dart';
+import 'package:widget_flutter_utils/widget/List/Slimy_Card_Widget.dart';
  
 void main() => runApp(MyApp());
  
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  List lista = [1,2,3,3];
+  // List lista = [1,2,3,3];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
         home: Scaffold(
-          body: ListViewPage(lista: lista),
-        ),
-    );
+          body: SlimyCardPage(),
+          )
+        );
   }
 }
---
+++ lib/widget/List/Slimy_Card_Widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:slimy_card/slimy_card.dart';
+
+class SlimyCardPage extends StatelessWidget {
+
+  // const SlimyCardPage(
+  //   {Key key}
+  //   );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        // This streamBuilder reads the real-time status of SlimyCard.
+        initialData: false,
+        stream: slimyCard.stream, //Stream of SlimyCard
+        builder: ((BuildContext context, AsyncSnapshot snapshot) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SizedBox(height: 100),
+
+              // SlimyCard is being called here.
+              SlimyCard(
+                // In topCardWidget below, imagePath changes according to the
+                // status of the SlimyCard(snapshot.data).
+                topCardWidget: topCardWidget((snapshot.data)
+                                    ? 'assets/yo.jpg'
+                                    : 'assets/gitana.jpg'),
+                                bottomCardWidget: bottomCardWidget(),
+                                ),
+                              ],
+                            );
+                          }),
+                        ),
+                      );
+                  }
+
+        
+topCardWidget(String imagePath) {
+     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            image: DecorationImage(image: AssetImage(imagePath)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'The Rock',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'He asks, what your name is. But!',
+          style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+              fontSize: 12,
+              fontWeight: FontWeight.w500),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+}
+                
+Widget bottomCardWidget() {
+    return Text(
+      'It doesn\'t matter \nwhat your name is.',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+}
